1e48d41bc277dca910907df52e48af91c74f8474
Chung jaemin <hijam76@gmail.com>
2017-12-21 15:14:51 +0900

    다음만화 내려받기 미완성이지만 일단 추가

--- __init__.py
+++ __init__.py
@@ -67,6 +67,7 @@ def create_app(config_filepath='resource/config.cfg'):
     # 뷰 함수 모듈은 어플리케이션 객체 생성하고 블루프린트 등록전에 
     # 뷰 함수가 있는 모듈을 임포트해야 해당 뷰 함수들을 인식할 수 있음
     from finance.controller import index
+    from finance.controller import daum
     
     from finance.finance_blueprint import finance
     finance_app.register_blueprint(finance)
---
+++ controller/daum.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+    finance.controller.daum
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    다음 만화 내려받기
+"""
+
+
+from flask import render_template, request, current_app, session, redirect \
+                 , url_for
+from functools import wraps
+from werkzeug import check_password_hash
+from wtforms import Form, TextField, PasswordField, HiddenField, validators
+
+from finance.database import dao
+from finance.finance_logger import Log
+from finance.finance_blueprint import finance
+import urllib2, re, os, json
+from os import path
+
+def saveToon(filename, imgURL):
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:42.0) Gecko/20100101 Firefox/42.0',
+       'Accept': 'image/png,image/*;q=0.8,*/*;q=0.5',
+       'Accept-Encoding': 'gzip, deflate',
+       'Accept-Language': 'en-US,en;q=0.5',
+       'Referer': 'http://webtoon.daum.net/#day=wed&tab=day',
+       'Connection': 'keep-alive'}
+
+    req = urllib2.Request(imgURL, headers=hdr)
+
+    try:
+        img = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        print e.fp.read()
+
+    file = open(filename, 'wb')
+    file.write(img.read())
+    file.close()
+
+def collectCartoonList(url, title) :
+    if not os.access(title, os.F_OK):
+        os.makedirs(title)
+        
+    listhtml = urllib2.urlopen('%sview/%s?timeStamp=' % (url, title)).read()
+    listjson = json.loads(listhtml)
+    ids = []
+    for episode in listjson['data']['webtoon']['webtoonEpisodes']:
+        ids.append(episode['id'])
+    ids.sort()
+    total = len(ids)
+    print('Total episode count : %s' % total)
+    
+    for no, epid in enumerate(ids):
+        ephtml = urllib2.urlopen('%s/viewer_images/%s' % (url, epid)).read()
+        epjson = json.loads(ephtml)
+        for epimage in epjson['data']:
+            fileName = '%s/%s%s.png' % (title, str(no+1).zfill(4), str(epimage['imageOrder']).zfill(2))
+            saveToon(fileName, epimage['url'])
+        print('Episode %s/%s saved' % (no+1, total))
+"""        
+# need to be edited
+titleId = 'game'
+baseurl = 'http://webtoon.daum.net/data/pc/webtoon/'
+collectCartoonList(baseurl, titleId)
+"""
+
+@finance.route('/daum')
+def daum():
+    """다음 만화 목록 표시해야하지만,
+       일단 만화 아이디 받아서 내려받기"""
+    return render_template('index.html')
